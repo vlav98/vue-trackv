d7f4a89f367b541b409d74b42579027d2141c6a5
Valentine <valentinelav98@gmail.com>
2021-09-20 00:03:30 +0200

    :art: Added tailwind theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      primary: "#eb6345",
+      primaryDull: "#fef7f5",
+      dark: "#d1583d",
+      darker: "#b84d35",
+      text: "#333",
+      white: "#FFFFFF",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
